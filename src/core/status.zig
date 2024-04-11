b7e1e0d75a32a0a3bea01cabcffafc337c6a19f4
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-04-11 15:54:19 -0700

    ISSUE: Status always returned modified.
    Need to fix that issue

--- src/core/status.zig
+++ src/core/status.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const blob = @import("../core/blob.zig");
 const tracked = @import("../core/tracked.zig");
+const cmp = @import("../util/compress.zig");
 
 const stdout = std.io.getStdOut().writer();
 
@@ -21,9 +22,38 @@ pub fn print(allocator: std.mem.Allocator) !void {
 
     // Read the file line by line
     while (try buf_reader.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        // Get data path of the blob
         const path = try blob.getDataPath(allocator, line);
-        const entry = try std.fmt.allocPrint(allocator, "->  {s}\n", .{path});
-        try stdout.writeAll(entry);
+
+        // Get the blob file
+        const blob_file = try blob.get(line);
+        defer blob_file.close();
+
+        // NOTE!: It might be faster to compress the new file instead...
+
+        // Open the path and compare if the decompressed blob is same as the file
+        const cur_file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
+        defer cur_file.close();
+
+        // Create writer and the decompressed blob
+        var list = std.ArrayList(u8).init(allocator);
+        try cmp.decompress(allocator, blob_file.reader(), list.writer());
+
+        const file_buf = try file.reader().readAllAlloc(allocator, 1024 * 1024);
+        const blob_buf = try list.toOwnedSlice();
+
+        // Compare the two buffers
+        if (std.mem.eql(u8, file_buf, blob_buf)) {
+            const entry = try std.fmt.allocPrint(allocator, "->  {s}\n", .{path});
+            try stdout.writeAll(entry);
+
+            // File has vbeen
+        } else {
+            const entry = try std.fmt.allocPrint(allocator, "MODIFIED!  {s}\n", .{path});
+            try stdout.writeAll(entry);
+        }
+
+        // Increment the number of files tracked
         i += 1;
     }
 
--- src/main.zig
+++ src/main.zig
@@ -14,46 +14,7 @@ pub fn main() !void {
     // try blob.new(allocator, "./build.zig");
     // try blob.new(allocator, "./src/main.zig");
     // try blob.new(allocator, "./src/core/blob.zig");
+    // try status.print(allocator);
     // try blob.new(allocator, "./src/util/compress.zig");
     // try blob.new(allocator, "./src/util/crypto.zig");
 }
-
-test "compressing files" {
-    const hello_input = try std.fs.cwd().openFile("./src/test_files/hello.txt", .{});
-    const lorem_input = try std.fs.cwd().openFile("./src/test_files/lorem.txt", .{});
-    defer {
-        hello_input.close();
-        lorem_input.close();
-    }
-
-    const hello_compressed = try std.fs.cwd().createFile("./src/test_files/hello_compressed.txt.z", .{ .truncate = true, .read = true });
-    const lorem_compressed = try std.fs.cwd().createFile("./src/test_files/lorem_compressed.txt.z", .{ .truncate = true, .read = true });
-    defer {
-        hello_compressed.close();
-        lorem_compressed.close();
-    }
-
-    try cmp.compress(std.testing.allocator, hello_input.reader(), hello_compressed.writer());
-    try cmp.compress(std.testing.allocator, lorem_input.reader(), lorem_compressed.writer());
-}
-
-// This fails and its not even my fault
-// Some kind of skill issue somewhere
-// test "decompressing files" {
-//     const hello_compressed = try std.fs.cwd().createFile("./src/test_files/hello_compressed.txt.z", .{ .truncate = true, .read = true });
-//     const lorem_compressed = try std.fs.cwd().createFile("./src/test_files/lorem_compressed.txt.z", .{ .truncate = true, .read = true });
-//     defer {
-//         hello_compressed.close();
-//         lorem_compressed.close();
-//     }
-//
-//     const hello_decompressed = try std.fs.cwd().createFile("./src/test_files/hello_decompressed.txt", .{ .truncate = true, .read = true });
-//     const lorem_decompressed = try std.fs.cwd().createFile("./src/test_files/lorem_decompressed.txt", .{ .truncate = true, .read = true });
-//     defer {
-//         hello_decompressed.close();
-//         lorem_decompressed.close();
-//     }
-//
-//     try cmp.decompress(std.testing.allocator, hello_compressed.reader(), hello_decompressed.writer());
-//     try cmp.decompress(std.testing.allocator, lorem_compressed.reader(), lorem_decompressed.writer());
-// }
